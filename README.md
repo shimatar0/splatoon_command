a59a1b44109d42105f15d403caec0c265afc5e32
shimataroo <shimataro0579@gmail.com>
2020-10-25 23:57:39 +0900

    武器マスタ取得コマンドのコミット

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # splatoon_command
+
+#### WeaponDataGetCommand
+
+武器マスタ生成のコマンド
+
+指定フォルダ(data/weapon/)のcsvファイルから武器のデータをDBに格納する。保存されるデータは元ファイルorスキーマ参照。  
+スプラ2はアップデートされないので、一度しか実行されないであろう悲しいコマンド(そして実行後にdumpしたsqlファイルがdata/weaponに…)    
+
+```
+python3 WeaponDataGetCommand.py
+```
--- src/command/WeaponDataGetCommand.py
+++ src/command/WeaponDataGetCommand.py
@@ -1,20 +1,34 @@
-#import mysql.connector
+import mysql.connector
 import csv
-
-#conn = mysql.connector.connect(host='127.0.0.1')
-
-#cursor = conn.cursor()
 
 # データ読み込み
 def createWeapon():
-    with open('../../data/weapon/statink-weapon2.csv', 'r') as f:
+    with open('../../data/weapon/weapon2.csv', 'r') as f:
         reader = csv.reader(f)
         # ラベルはスキップ
         header = next(reader)
+        conn = mysql.connector.connect(
+                                user='root',  
+                                host='localhost',
+                                database='splatoon'  
+                            )
+        cursor = conn.cursor()
         for row in reader:
-            print(row[2])
+            category = row[0]
+            full_name = row[5]
+            short_name = row[6]
+            sub_weapon = row[7]
+            special = row[8]
+            main_performance_up = row[11]
+            
+        # DB保存
+            sql = 'INSERT INTO weapon (category, full_name, short_name, sub_weapon, special, main_performance_up) VALUES (%s, %s, %s, %s, %s, %s)'
+            cursor.execute(sql, (category, full_name, short_name, sub_weapon, special, main_performance_up))
 
-# DB保存
+        conn.commit()
+        cursor.close()
+        conn.close()
+
 
 # main
 if __name__ == "__main__":
